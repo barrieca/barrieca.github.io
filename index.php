a9790b6e64acd23354e175f01a97e6f5af2d2b3d
barrieca <cambarrie@comcast.net>
2019-11-22 15:46:34 -0600

    Added some of my old files to be updated.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CS 290</title>
+<base href="http://web.engr.oregonstate.edu/~barrieca/cs290/">
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include('nav'); ?>
+<section>
+<h3>CS 290</h3>
+<br><br><br>
+Cameron Barrie
+</section>
+<?php include('footer'); ?>
+</body>
+</html>
